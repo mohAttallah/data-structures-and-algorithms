c46648471d7d398c1887a46d39fb8dc137dbafe6
mohAttallah <mohmohmoud28@gmail.com>
2023-07-17 20:47:54 +0000

    still working with lab

---
+++ javascript/code-challenges/trees15/BinaryTree.js
@@ -0,0 +1,22 @@
+'use strict';
+
+class BinaryTree{
+    constructor(root = null) {
+        this.root = root;
+    }
+    // root >> left >> right 
+    preOrder() {
+        let arr = [];
+        const traversal = node => {
+            arr.push(node.value);
+            if()
+        }
+    }
+    postOrder() {
+        
+    }
+    inOrder() {
+        
+    }
+}
+
