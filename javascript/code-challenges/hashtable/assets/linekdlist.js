a0789bf3ae21402cdda7165c3eef72bc99034f61
mohAttallah <mohmohmoud28@gmail.com>
2023-08-31 20:37:33 +0000

    create hash table class

---
+++ javascript/code-challenges/hashtable/assets/linekdlist.js
@@ -0,0 +1,22 @@
+'use strict';
+const Node = require('./node');
+class LinkedList{
+    constructor() {
+        this.head = null;
+    }
+    insert(value) {
+        const newNode = new Node(value);
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+                
+            }
+            current.next = newNode;
+        }
+    }
+}
+
+module.exports = LinkedList; 
