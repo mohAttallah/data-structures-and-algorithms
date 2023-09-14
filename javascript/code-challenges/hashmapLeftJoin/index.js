9078ecd0cac01647924fa25579aed1f019ef6381
mohAttallah <mohmohmoud28@gmail.com>
2023-09-14 22:40:49 +0300

    left join

--- javascript/code-challenges/hashmapLeftJoin/index.js
+++ javascript/code-challenges/hashmapLeftJoin/index.js
@@ -1,4 +1,32 @@
-function leftJoin(map1, map2) {
-    
-    return 
+"use strict"
+function leftJoin(maps1, maps2) {
+    const results = [];
+
+    maps2.forEach((value2, key) => {
+        let value1 = maps1.get(key);
+        if (value1 !== undefined) {
+            results.push([value1, value2]);
+        }
+
+    });
+    return results;
 }
+
+module.exports = leftJoin;
+
+const maps1 = new Map();
+
+maps1.set('diligent', 'employed');
+maps1.set('fond', 'enamored');
+maps1.set('guide', 'usher');
+maps1.set('outfit', 'garb');
+maps1.set('wrath', 'anger');
+
+const maps2 = new Map();
+maps2.set('diligent', 'idle');
+maps2.set('fond', 'averse');
+maps2.set('guide', 'follow');
+maps2.set('flow', 'jam');
+maps2.set('wrath', 'delight');
+
+console.log(leftJoin(maps1, maps2))
---
+++ javascript/code-challenges/hashmapLeftJoin/readme.md
@@ -0,0 +1,17 @@
+# left join
+Write a function that LEFT JOINs two hashmaps into a single data structure.
+
+
+### Description 
+Write a function called left join
+**Arguments**: *two hash maps*
+The first parameter is a hashmap that has word strings as keys, and a synonym of the key as values.
+The second parameter is a hashmap that has word strings as keys, and antonyms of the key as values.
+**Return**: *array of results after join*
+
+### whiteboard
+![whiteboard](./left-join.jpg)
+
+### code & unit test
+[code](./index.js)
+[Unit test](./__test__/leftJoin.test.js)
